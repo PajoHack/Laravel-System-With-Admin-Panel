a9354719c553bbd8d23ec86a6a1d5c534e75dbc7
Pajo <pajohack@gmail.com>
2018-01-06 15:43:17 +0000

    Persisting Users to DB tested & is working

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,46 @@
-<h1>Admin Users Index</h1>
+@extends('layouts.admin')
+
+
+
+@section('content')
+
+    <h1>Users</h1>
+
+    <table class="table">
+
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+              <th>Role</th>
+              <th>Status</th>
+              <th>Created</th>
+              <th>Updated</th>
+          </tr>
+        </thead>
+
+        @if($users)
+
+            @foreach($users as $user)
+
+        <tbody>
+          <tr>
+            <td>{{ $user->id }}</td>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+              <td>{{ $user->role->name }}</td>
+              {{--<td>{‌{$user->role->name == $user->role->name ? $user->role->name : 'User has no role'}}</td>--}}
+              <td>{{ $user->is_active == 1 ? 'Active' : 'Not Active' }}</td>
+              <td>{{ $user->created_at->diffForHumans() }}</td>
+              <td>{{ $user->updated_at->diffForHumans() }}</td>
+          </tr>
+        </tbody>
+
+            @endforeach
+
+        @endif
+
+      </table>
+
+    @endsection
